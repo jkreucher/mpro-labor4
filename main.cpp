0ff53999c59acbc5969bbb8cc84d5de21448925f
Jannik Kreucher <jannik.kreucher@th-bingen.de>
2022-06-19 20:04:23 +0200

    cooperative multitasking

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@
 #define SERVO_PERIOD_MIN    1100 //in us
 
 
+// Input / Output
 PwmOut pinServo(PIN_SERVO);
 DigitalOut pinLedOrange1(LED_ORANGE1);
 DigitalOut pinLedOrange2(LED_ORANGE2);
@@ -45,33 +46,48 @@ DigitalIn pinSW3(SW_3);
 DigitalIn pinSW4(SW_4);
 DigitalIn pinSW5(SW_5);
 
+// timer interrupt
 Ticker sysTick;
 
+// task struct
+struct stTask {
+    void (*handler)();
+    uint16_t period;
+    uint16_t counter;
+};
+
+// global variables
 int32_t servoPeriod = 0;
 int32_t servoPeriodSave[2];
 volatile uint8_t standby = 0;
 uint16_t counterSW3 = 0, counterSW4 = 0;
 
 
-void vTasten() {
-    // check switch 2
-    if(pinSW2 == 1) {
-        servoPeriod += 10;
-        pinLedOrange1 = 1;
-        pinLedGreen2 = 0;
-    } else {
-        pinLedOrange1 = 0;
-    }
+void vButtonsPosition() {
+    // check if in program mode
+    if(pinSW1 == 1) {
+        // check switch 2
+        if(pinSW2 == 1) {
+            servoPeriod += 10;
+            pinLedOrange2 = 1;
+            pinLedGreen2 = 0;
+        } else {
+            pinLedOrange2 = 0;
+        }
 
-    // check switch 5
-    if(pinSW5 == 1) {
-        servoPeriod -= 10;
-        pinLedOrange2 = 1;
-         pinLedGreen2 = 0;
-    } else {
-        pinLedOrange2 = 0;
+        // check switch 5
+        if(pinSW5 == 1) {
+            servoPeriod -= 10;
+            pinLedOrange1 = 1;
+            pinLedGreen2 = 0;
+        } else {
+            pinLedOrange1 = 0;
+        }
     }
+}
 
+
+void vButtonsMemory() {
     // check switch 3
     if(pinSW3 == 1) {
         // button pressed
@@ -82,9 +98,12 @@ void vTasten() {
                 // long press
                 // set counter to one above short press so this does not get executed one more time
                 counterSW3++;
-                // save servo position
-                servoPeriodSave[0] = servoPeriod;
-                pinLedRed2 = 1;
+                // check if in program mode
+                if(pinSW1 == 1) {
+                    // save servo position
+                    servoPeriodSave[0] = servoPeriod;
+                    pinLedRed2 = 1;
+                }
             }
         }
     } else {
@@ -113,9 +132,12 @@ void vTasten() {
                 // long press
                 // set counter to one above short press so this does not get executed one more time
                 counterSW4++;
-                // save servo position
-                servoPeriodSave[1] = servoPeriod;
-                pinLedRed2 = 1;
+                // check if in program mode
+                if(pinSW1 == 1) {
+                    // save servo position
+                    servoPeriodSave[1] = servoPeriod;
+                    pinLedRed2 = 1;
+                }
             }
         }
     } else {
@@ -134,12 +156,34 @@ void vTasten() {
         counterSW4 = 0;
     }
 
-
     // clamp servo position
     if(servoPeriod > SERVO_PERIOD_MAX) servoPeriod = SERVO_PERIOD_MAX;
     if(servoPeriod < SERVO_PERIOD_MIN) servoPeriod = SERVO_PERIOD_MIN;
     // set servo period
     pinServo.pulsewidth_us(servoPeriod);
+}
+
+
+// task list
+struct stTask schedulerTasks[] = {
+    {vButtonsMemory, 10, 0},
+    {vButtonsPosition, 20, 0}
+};
+
+void vScheduler() {
+    // check for each task if it needs to be called
+    for(uint8_t taskid=0; taskid < (sizeof(schedulerTasks)/sizeof(stTask)); taskid++) {
+        if(schedulerTasks[taskid].counter == 0) {
+            // time has come
+            // reset counter
+            schedulerTasks[taskid].counter = schedulerTasks[taskid].period;
+            // call task handler
+            schedulerTasks[taskid].handler();
+        } else {
+            // decrement task counter
+            schedulerTasks[taskid].counter--;
+        }
+    }
 }
 
 
@@ -151,12 +195,14 @@ void vTick() {
 int main() {
     // configure servo frequency to 50Hz
     pinServo.period_ms(20);
+
     // time slice interrupt
-    sysTick.attach(&vTick, 10ms);
+    sysTick.attach(&vTick, 1ms);
 
 	while(1) {
-        // check buttons and switches
-        vTasten();
+        // execute scheduler every 1ms
+        // this function checks if a task needs to be executed
+        vScheduler();
 
         // wait for time slice
         standby = 1;
