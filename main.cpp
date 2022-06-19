357db513a7fa566596afd5641f20451a5d600981
Jannik Kreucher <jannik.kreucher@th-bingen.de>
2022-06-19 17:26:23 +0200

    Servo Test

--- main.cpp
+++ main.cpp
@@ -23,12 +23,31 @@
 #define SW_3    PB_6
 #define SW_4    PA_7
 #define SW_5    PA_6
+#define PIN_SERVO   PB_10
+
+
+
+PwmOut pinServo(PIN_SERVO);
+DigitalOut ledOrange1(LED_ORANGE1);
+DigitalOut ledOrange2(LED_ORANGE2);
 
 
 int main() {
+    // configure servo frequency to 50Hz
+    pinServo.period_ms(20);
 
 	while(1) {
+        pinServo.pulsewidth_us(1100);
+        ledOrange1 = 1;
+        ledOrange2 = 0;
 
+        ThisThread::sleep_for(2s);
+
+        pinServo.pulsewidth_us(2100);
+        ledOrange1 = 0;
+        ledOrange2 = 1;
+
+        ThisThread::sleep_for(2s);
 	}
 
 	return 0;
