50299d83a1e6a93ceca318055496345a555c2daf
Jannik Kreucher <jannik.kreucher@th-bingen.de>
2022-06-19 18:04:59 +0200

    basic key input working

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
  *             Benedikt Wendling
  */
 #include "mbed.h"
+#include <algorithm>
+#include <cstdint>
 
 
 /*** PIN DEFINITIONS ***/
@@ -25,29 +27,111 @@
 #define SW_5    PA_6
 #define PIN_SERVO   PB_10
 
+#define CYCLE_TIME  10ms
+#define PRESS_SHORT 40
+
+#define SERVO_PERIOD_MAX    2100
+#define SERVO_PERIOD_MIN    1100
 
 
 PwmOut pinServo(PIN_SERVO);
-DigitalOut ledOrange1(LED_ORANGE1);
-DigitalOut ledOrange2(LED_ORANGE2);
+DigitalOut pinLedOrange1(LED_ORANGE1);
+DigitalOut pinLedOrange2(LED_ORANGE2);
+DigitalIn pinSW1(SW_1);
+DigitalIn pinSW2(SW_2);
+DigitalIn pinSW3(SW_3);
+DigitalIn pinSW4(SW_4);
+DigitalIn pinSW5(SW_5);
+
+Ticker sysTick;
+
+int32_t servoPeriod = 0;
+int32_t servoPeriodSave[2];
+volatile uint8_t standby = 0;
+uint16_t counterSW3 = 0, counterSW4 = 0;
+
+
+void vTasten() {
+    // check switch 2
+    if(pinSW2 == 1) {
+        servoPeriod += 10;
+    }
+    // check switch 5
+    if(pinSW5 == 1) {
+        servoPeriod -= 10;
+    }
+    // check switch 3
+    if(pinSW3 == 1) {
+        // button pressed
+        if(counterSW3 < PRESS_SHORT) {
+            counterSW3++;
+        } else {
+            if(counterSW3 == PRESS_SHORT) {
+                // long press
+                // set counter to one above short press so this does not get executed one more time
+                counterSW3++;
+                // do stuff with long press here
+                servoPeriodSave[0] = servoPeriod;
+            }
+        }
+    } else {
+        // button not pressed (released)
+        if((counterSW3 < PRESS_SHORT) && (counterSW3 != 0)) {
+            // short press detected
+            servoPeriod = servoPeriodSave[0];
+        }
+        counterSW3 = 0;
+    }
+    // check switch 4
+    if(pinSW4 == 1) {
+        // button pressed
+        if(counterSW4 < PRESS_SHORT) {
+            counterSW4++;
+        } else {
+            if(counterSW4 == PRESS_SHORT) {
+                // long press
+                // set counter to one above short press so this does not get executed one more time
+                counterSW4++;
+                // do stuff with long press here
+                servoPeriodSave[1] = servoPeriod;
+            }
+        }
+    } else {
+        // button not pressed (released)
+        if((counterSW4 < PRESS_SHORT) && (counterSW4 != 0)) {
+            // short press detected
+            servoPeriod = servoPeriodSave[1];
+        }
+        counterSW4 = 0;
+    }
+
+
+    // clamp servo position
+    if(servoPeriod > SERVO_PERIOD_MAX) servoPeriod = SERVO_PERIOD_MAX;
+    if(servoPeriod < SERVO_PERIOD_MIN) servoPeriod = SERVO_PERIOD_MIN;
+    // set servo period
+    pinServo.pulsewidth_us(servoPeriod);
+}
+
+
+void vTick() {
+    standby = 0;
+}
 
 
 int main() {
     // configure servo frequency to 50Hz
     pinServo.period_ms(20);
+    // time slice interrupt
+    sysTick.attach(&vTick, 10ms);
 
 	while(1) {
-        pinServo.pulsewidth_us(1100);
-        ledOrange1 = 1;
-        ledOrange2 = 0;
+        // check buttons and switches
+        vTasten();
 
-        ThisThread::sleep_for(2s);
-
-        pinServo.pulsewidth_us(2100);
-        ledOrange1 = 0;
-        ledOrange2 = 1;
-
-        ThisThread::sleep_for(2s);
+        // wait for time slice
+        standby = 1;
+        while(standby);
 	}
 
 	return 0;
